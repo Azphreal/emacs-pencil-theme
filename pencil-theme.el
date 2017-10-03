5103dae34a0f629f9f239bd836275b2bf8b86a56
Alex Smith <azphreal19@protonmail.com>
2017-10-03 16:47:52 +1100

    Add flycheck and org-mode
    Also use pink brackets everywhere

--- pencil-theme.el
+++ pencil-theme.el
@@ -82,8 +82,6 @@
       (custom-theme-set-faces
        'pencil
 
-
-
        ;; Default
        `(default ((t (:background ,bg :foreground ,fg))))
        `(bold ((t (:weight bold))))
@@ -98,9 +96,13 @@
        `(link-visited ((t (:foreground ,purple))))
        `(minibuffer-prompt ((t (:foreground ,green :weight normal))))
 
+       ;; which-function
+       `(which-func ((t (:foreground ,blue :background nil))))
+
        ;; linum-mode
        `(linum ((t (:foreground ,bg-subtle :background ,bg))))
        `(linum-current-line ((t (:foreground ,blue :background ,bg))))
+       `(linum-highlight-face ((t (:foreground ,blue :background ,bg))))
 
        ;; Mode line
        `(mode-line ((t (:background ,bg-subtle :box nil))))
@@ -173,6 +175,16 @@
        ;; `(company-tooltip-search ((t ())))
        `(company-tooltip-selection ((t (:weight normal :foreground ,fg :background ,blue))))
 
+       ;; Flycheck
+       `(flycheck-error ((t (:underline (:style wave :color ,red)))))
+       `(flycheck-info ((t (:underline (:style wave :color ,blue)))))
+       `(flycheck-warning ((t (:underline (:style wave :color ,orange)))))
+       `(flycheck-fringe-error ((t (:foreground ,red :background ,bg-very-subtle))))
+       `(flycheck-fringe-info ((t (:foreground ,blue :background ,bg-very-subtle))))
+       `(flycheck-fringe-warning ((t (:foreground ,yellow :background ,bg-very-subtle))))
+       ;; Flymake
+       `(flymake-warnline ((t (:underline (:style wave :color ,orange) :background ,bg))))
+       `(flymake-errline ((t (:underline (:style wave :color ,red) :background ,bg))))
 
        ;; Powerline
        `(powerline-active1 ((t (:foreground ,fg :background ,bg-subtle))))
@@ -194,19 +206,72 @@
        `(spacemacs-replace-face ((t (:inherit spaceline-evil-visual))))
 
        ;; Rainbow-delimiters
-       `(rainbow-delimiters-depth-1-face ((t (:foreground ,pink))))
-       `(rainbow-delimiters-depth-2-face ((t (:foreground ,blue))))
-       `(rainbow-delimiters-depth-3-face ((t (:foreground ,orange))))
-       `(rainbow-delimiters-depth-4-face ((t (:foreground ,purple))))
-       `(rainbow-delimiters-depth-5-face ((t (:foreground ,green))))
-       `(rainbow-delimiters-depth-6-face ((t (:foreground ,cyan))))
-       `(rainbow-delimiters-depth-7-face ((t (:foreground ,dark-red))))
-       `(rainbow-delimiters-depth-8-face ((t (:foreground ,dark-yellow))))
-       `(rainbow-delimiters-depth-9-face ((t (:foreground ,yellow))))
+       ;`(rainbow-delimiters-depth-1-face ((t (:foreground ,pink))))
+       ;`(rainbow-delimiters-depth-2-face ((t (:foreground ,blue))))
+       ;`(rainbow-delimiters-depth-3-face ((t (:foreground ,orange))))
+       ;`(rainbow-delimiters-depth-4-face ((t (:foreground ,purple))))
+       ;`(rainbow-delimiters-depth-5-face ((t (:foreground ,green))))
+       ;`(rainbow-delimiters-depth-6-face ((t (:foreground ,cyan))))
+       ;`(rainbow-delimiters-depth-7-face ((t (:foreground ,dark-red))))
+       ;`(rainbow-delimiters-depth-8-face ((t (:foreground ,dark-yellow))))
+       ;`(rainbow-delimiters-depth-9-face ((t (:foreground ,yellow))))
+       `(rainbow-delimiters-depth-1-face ((t (:foreground ,dark-red))))
+       `(rainbow-delimiters-depth-2-face ((t (:foreground ,pink))))
+       `(rainbow-delimiters-depth-3-face ((t (:foreground ,pink))))
+       `(rainbow-delimiters-depth-4-face ((t (:foreground ,pink))))
+       `(rainbow-delimiters-depth-5-face ((t (:foreground ,pink))))
+       `(rainbow-delimiters-depth-6-face ((t (:foreground ,pink))))
+       `(rainbow-delimiters-depth-7-face ((t (:foreground ,pink))))
+       `(rainbow-delimiters-depth-8-face ((t (:foreground ,pink))))
+       `(rainbow-delimiters-depth-9-face ((t (:foreground ,pink))))
        `(rainbow-delimiters-unmatched-face ((t (:foreground ,fg :background ,orange))))
 
        ;; rust-mode
        `(rust-question-mark-face ((t (:foreground ,pink :inherit font-lock-builtin-face))))
+
+       ;; org-mode
+       `(org-agenda-structure ((t (:foreground ,blue :bold t))))
+       `(org-agenda-date ((t (:foreground ,fg :underline nil))))
+       `(org-agenda-done ((t (:foreground ,green))))
+       `(org-agenda-dimmed-todo-face ((t (:foreground ,comment))))
+       `(org-block ((t (:foreground ,fg :background ,bg-very-subtle))))
+       `(org-block-background ((,t (:background ,bg-very-subtle))))
+       `(org-code ((t (:foreground ,red :background ,bg))))
+       `(org-verbatim ((t (:foreground ,dark-blue :inherit org-code))))
+       ;`(org-column ((t (:background ,current-line))))
+       ;`(org-column-title ((t (:inherit org-column :weight bold :underline t))))
+       `(org-date ((t (:foreground ,pink :underline t))))
+       `(org-document-info ((t (:foreground ,fg-subtle :height 1.2))))
+       `(org-document-info-keyword ((t (:foreground ,purple :height 1.2))))
+       `(org-document-title ((t (:weight bold :foreground ,fg :height 1.2))))
+       `(org-done ((t (:bold t :foreground ,green))))
+       `(org-ellipsis ((t (:foreground ,comment))))
+       `(org-footnote ((t (:foreground ,blue))))
+       `(org-formula ((t (:foreground ,red))))
+       `(org-hide ((t (:foreground ,fg :background ,fg))))
+       `(org-link ((t (:foreground ,red :underline t))))
+       `(org-scheduled ((t (:foreground ,green))))
+       `(org-scheduled-previously ((t (:foreground ,orange))))
+       `(org-scheduled-today ((t (:foreground ,green))))
+       `(org-special-keyword ((t (:foreground ,comment))))
+       `(org-table ((t (:foreground ,dark-blue :background ,bg-very-subtle))))
+       `(org-todo ((t (:bold t :foreground ,red))))
+       `(org-upcoming-deadline ((t (:foreground ,orange))))
+       `(org-warning ((t (:weight bold :foreground ,red))))
+       `(org-block-begin-line ((t (:foreground ,bg-subtle :background ,bg))))
+       `(org-block-end-line ((t (:foreground ,bg-subtle :background ,bg))))
+       `(org-kbd ((t (:background ,comment :foreground ,fg
+                                       :box (:line-width 1 :color nil :style pressed-button)))))
+
+       `(org-level-1 ((t (:inherit outline-1 :foreground ,blue))))
+       `(org-level-2 ((t (:inherit outline-2 :foreground ,cyan))))
+       `(org-level-3 ((t (:inherit outline-3 :foreground ,dark-blue))))
+       `(org-level-4 ((t (:inherit outline-4 :foreground ,green))))
+       `(org-level-5 ((t (:inherit outline-5 :foreground ,red))))
+       `(org-level-6 ((t (:inherit outline-6 :foreground ,purple))))
+       `(org-level-7 ((t (:inherit outline-7 :foreground ,pink))))
+       `(org-level-8 ((t (:inherit outline-8))))
+       `(org-level-9 ((t (:inherit outline-9))))
 
        ))))
 (provide-theme 'pencil)
